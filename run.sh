58023cfdf54a75b28dd9e34067da4d792a3e9004
sabbih-shah <66474019+sabbih-shah@users.noreply.github.com>
2024-12-23 07:25:30 +1100

    Added sample run file

---
+++ run.sh
@@ -0,0 +1,6 @@
+python eventset_generation.py \
+  --start_time 2023-01-01 \
+  --end_time 2023-01-07 \
+  --ensemble_size 1000 \
+  --input_data_location "/pscratch/sd/s/sabbih/scs_era5_v1.1/conus/2023/*.nc" \
+  --output_location "pscratch/sd/s/sabbih/weather_forcasting/event_set/2023-01-01_2023-01-07.zarr"
