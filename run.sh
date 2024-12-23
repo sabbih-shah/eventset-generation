f023348db9e40ab814be35cbfbc6939a498fed83
sabbih-shah <66474019+sabbih-shah@users.noreply.github.com>
2024-12-23 18:51:59 +1100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ python eventset_generation.py \
   --ensemble_size 1000 \
   --input_data_location "/pscratch/sd/s/sabbih/scs_era5_v1.1/conus/2023/*.nc" \
   --output_location "/pscratch/sd/s/sabbih/weather_forcasting/event_set/2023-01-01_2023-01-07.zarr" \
-  --lognorm_shape_file "pscratch/sd/s/sabbih/weather_forcasting/event_set/lognorm_shape.nc" \
+  --lognorm_shape_file "/pscratch/sd/s/sabbih/weather_forcasting/event_set/lognorm_shape.nc" \
   --lognorm_scale_file "/pscratch/sd/s/sabbih/weather_forcasting/event_set/lognorm_scale.nc"
